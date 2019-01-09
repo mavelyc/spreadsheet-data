a98cc7e299b3710159fa72b4c0afaeaf54d38dcd
mavelyc <mavelyc@mcmaster.ca>
2019-01-09 13:09:40 -0500

    write and read successfully

--- chumparser.py
+++ chumparser.py
@@ -2,27 +2,22 @@ import xlrd
 import xlsxwriter
 
 #intial write to new excel file
-workbook = xlsxwriter.Workbook('test.xlsx')
+workbook = xlsxwriter.Workbook('final.xlsx')
 worksheet = workbook.add_worksheet()
 
-worksheet.write(0,0,'Hello')
-
-workbook.close()
-
-
-#set row for headers
-""" row = sheet.row(0)
-row.write(0,'Date')
-row.write(1,'Numbers')
-row.write(2, 'degrees')
-row.write(3,'percent1')
-row.write(4,'percent2') """
+worksheet.write(0,0,'Date')
+worksheet.write(0,1,'Number')
+worksheet.write(0,2,'Degrees')
+worksheet.write(0,3,'Percent1')
+worksheet.write(0,4,'Percent2')
 
 
 #read values
-""" book = xlrd.open_workbook('Cx_Data SYS01 CTA Alim T4 H4 Sept1.xls')
+book = xlrd.open_workbook('Cx_Data SYS01 CTA Alim T4 H4 Sept1.xls')
 sheet = book.sheet_by_index(0)
-i = sheet.cell(7,1).value """
-#j = sheet.cell(1,1).value
+i = sheet.cell(7,1).value
+j = sheet.cell(1,1).value
+print i
+worksheet.write(1,0,str(i))
 
-#print i
+workbook.close()
