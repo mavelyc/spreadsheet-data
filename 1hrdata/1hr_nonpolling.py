83fa6d33b75fd9cbc9f2a45341144134ce84fa8e
mavelyc <mavelyc@mcmaster.ca>
2019-01-21 16:11:06 -0500

    first value is averaged

--- 1hrdata/1hr_nonpolling.py
+++ 1hrdata/1hr_nonpolling.py
@@ -1,12 +1,13 @@
 #used to summarize data given at random times into hourly increment
 import csv
 import xlsxwriter
+import xlrd
 
 #intial write to new excel file
-workbook = xlsxwriter.Workbook('1hr_final_nonpoll.xlsx')
+workbook = xlsxwriter.Workbook('1hr_tmp_nonpoll.xlsx')
 worksheet = workbook.add_worksheet()
 
-with open('TL_B10670CTA02_T_Alim_T4_TL.csv') as csv_file:
+with open('TL_B10670CTA01_Deb_VAlim_FMS1A_TL.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line = 0
     for time,value in csv_reader:
@@ -20,46 +21,8 @@ with open('TL_B10670CTA02_T_Alim_T4_TL.csv') as csv_file:
 
 for i in range(1,6001):
     formula = '=HOUR(A' + str(i+1) + ')'
+    formula2 = '=TEXT(A' + str(i+1) + ','+'"mm/dd/yy"'+')'
     worksheet.write(i,2,formula)
-
-count = 1
-while(count<6002):
-    
-        
-
-
-# worksheet.set_column(0,0,25)
-# worksheet.write(0,0,'Time')
-# worksheet.write(0,1,'Data')
-
-# cell_format = workbook.add_format()
-# cell_format.set_num_format('dd/mm/yyyy hh:mm')
-
-# #read values
-# k = 1
-# for f in files:
-#     book = xlrd.open_workbook(f)
-#     sheet = book.sheet_by_index(0)
-#     i = 7
-#     j = 0
-#     while (i<295):
-#         if j==0:
-#             worksheet.write(k,j,sheet.cell(i,j).value, cell_format)
-#         else: 
-#             total = 0
-#             count = i
-#             for num in range(12):
-#                 tmp = sheet.cell(count,j).value
-#                 tmp = tmp.encode('ascii')
-#                 tmp = float(tmp)
-#                 total= total + tmp
-#                 count+=1
-#             ave = total/12.0
-#             worksheet.write(k,j,ave)
-#         j+=1
-#         if j > 4:
-#             j=j%5
-#             i+=12
-#             k+=1
+    worksheet.write(i,3,formula2)
 
 workbook.close()
---
+++ 1hrdata/1hr_nonpolling2.py
@@ -0,0 +1,40 @@
+import xlsxwriter
+import xlrd
+
+workbook = xlsxwriter.Workbook('1hr_final_nonpoll.xlsx')
+worksheet = workbook.add_worksheet()
+
+worksheet.write(0,0,"Date")
+worksheet.write(0,1,"Time")
+worksheet.write(0,2,"Value")
+
+book = xlrd.open_workbook('1hr_tmp_nonpoll.xlsx')
+sheet = book.sheet_by_index(0)
+
+val = sheet.cell(1,2).value
+count = 1
+iterate = 1
+total=0
+while (iterate<6001):
+    if (sheet.cell(iterate,2).value == val):
+        tmp = sheet.cell(iterate,1).value
+        tmp = tmp.encode('ascii')
+        tmp = float(tmp)
+        total += tmp
+        iterate+=1
+    else:
+        worksheet.write(count,1,val)
+        #print iterate,count
+        final = total/(iterate-count)
+        worksheet.write(count,2,final)
+        count=iterate+1
+        iterate=count
+        val = sheet.cell(count,2).value
+
+worksheet.write(count,1,val)
+#print iterate,count
+final = total/(iterate-count)
+worksheet.write(count,2,final)
+
+        
+workbook.close()
