3c82d70ee0f582d2ea03d65c5e93c089d195230f
mavelyc <mavelyc@mcmaster.ca>
2019-02-01 13:42:19 -0500

    got it fully functioning, now refining

--- csv_combine/combine.py
+++ csv_combine/combine.py
@@ -16,10 +16,37 @@ for f in allFiles:
     df = pd.read_csv(f)
     df.rename(columns={"Value (%)":f[2:-4],"Value (°C)":f[2:-4]}, inplace=True)
     df.drop_duplicates(subset="Time", keep="first",inplace=True)
-    df.dropna(axis=0, how='any', thresh=None, subset=["Time"], inplace=True)
+    #df.dropna(axis=0, how='any', thresh=None, subset=["Time"], inplace=True)
     df = df.reset_index()
     df.sort_values(by=['Time'])
     df = df.drop(columns=["index"])
+    
+    df['Hour'] = pd.to_datetime(df['Time']).dt.hour
+    df["Ave_Time"] = ""
+    df["Ave_Val"] = ""
+    hour_val = df['Hour'][0]
+    total = 0
+    count = 0
+    ave_tally = 0
+    globindex=0
+    for index, row in df.iterrows():
+        if (row['Hour'] == hour_val):
+            total += row[f[2:-4]]
+            ave_tally+=1
+        else:
+            df.iloc[count, df.columns.get_loc('Ave_Time')] = df["Time"][index-1]
+            ave = total/ave_tally
+            df.iloc[count, df.columns.get_loc('Ave_Val')] = ave
+            hour_val = row['Hour']
+            total=row[f[2:-4]]
+            count += 1
+            ave_tally =1
+        globindex = index
+    globindex+=1
+    df.iloc[count, df.columns.get_loc('Ave_Time')] = df["Time"][globindex-1]
+    ave = total/ave_tally
+    df.iloc[count, df.columns.get_loc('Ave_Val')] = ave
+    df[""] = ""
     df_list.append(df)
 
 
--- csv_combine/test.py
+++ csv_combine/test.py
@@ -4,11 +4,34 @@ import pandas as pd
 
 df=pd.read_csv("TL_DS570CTA01_H_EvacAmRT_H5_TL.csv")
 df['Hour'] = pd.to_datetime(df['Time']).dt.hour
-#df.to_csv("help.csv")
+df[""] = ""
+df["Ave_Time"] = ""
+df["Val"] = ""
 # print(df['Hour'][5999])
 # if (pd.isna(df['Hour'][6000])): print ("True")
 # else: print ("False")
 # #print (df)
 
+hour_val = df['Hour'][0]
+total = 0
+count = 0
+ave_tally = 0
+globindex=0
 for index, row in df.iterrows():
-    print(index['Hour'],row['Hour'])
+    if (row['Hour'] == hour_val):
+        total += row['Value (%)']
+        ave_tally+=1
+    else:
+        df.iloc[count, df.columns.get_loc('Ave_Time')] = df["Time"][index-1]
+        ave = total/ave_tally
+        df.iloc[count, df.columns.get_loc('Val')] = ave
+        hour_val = row['Hour']
+        total=row['Value (%)']
+        count += 1
+        ave_tally =1
+    globindex = index
+globindex+=1
+df.iloc[count, df.columns.get_loc('Ave_Time')] = df["Time"][globindex-1]
+ave = total/ave_tally
+df.iloc[count, df.columns.get_loc('Val')] = ave
+df.to_csv("help.csv")
