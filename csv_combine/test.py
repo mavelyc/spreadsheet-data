50ba95e17bc91e913f7cc442d48a460d18f563e8
mavelyc <mavelyc@mcmaster.ca>
2019-02-01 09:52:31 -0500

    able to isolate hour from timestamp

---
+++ csv_combine/test.py
@@ -0,0 +1,9 @@
+#testing before implementation into combine.py
+
+import pandas as pd
+
+df=pd.read_csv("TL_DS570CTA01_H_EvacAmRT_H5_TL.csv")
+df['Hour'] = pd.to_datetime(df['Time']).dt.hour
+df.to_csv("help.csv")
+
+#print (df)
