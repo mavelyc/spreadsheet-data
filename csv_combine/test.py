f9e7000f241de12cb9a061e53c934bd2246db5ba
mavelyc <mavelyc@mcmaster.ca>
2019-02-01 10:42:35 -0500

    can iterate through entire hour column without error when null

--- csv_combine/test.py
+++ csv_combine/test.py
@@ -4,6 +4,11 @@ import pandas as pd
 
 df=pd.read_csv("TL_DS570CTA01_H_EvacAmRT_H5_TL.csv")
 df['Hour'] = pd.to_datetime(df['Time']).dt.hour
-df.to_csv("help.csv")
+#df.to_csv("help.csv")
+# print(df['Hour'][5999])
+# if (pd.isna(df['Hour'][6000])): print ("True")
+# else: print ("False")
+# #print (df)
 
-#print (df)
+for index, row in df.iterrows():
+    print(index['Hour'],row['Hour'])
